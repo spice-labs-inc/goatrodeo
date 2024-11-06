d917f839da78f0a22a545f41a6693b760dfe71f3
Brendan McAdams <brendan@spicelabs.io>
2024-11-06 13:16:18 -0800

    bump version in cli tool to match build.sbt

--- src/main/scala/goatrodeo/Main.scala
+++ src/main/scala/goatrodeo/Main.scala
@@ -68,7 +68,7 @@ object Howdy {
     import builder._
     OParser.sequence(
       programName("goatrodeo"),
-      head("goatrodeo", "0.4.6"),
+      head("goatrodeo", "0.5.0-SNAPSHOT"),
       opt[File]('a', "analyze")
         .action((x, c) =>
           c.copy(analyze = Some(x).filter(f => f.exists() && f.isFile()))
