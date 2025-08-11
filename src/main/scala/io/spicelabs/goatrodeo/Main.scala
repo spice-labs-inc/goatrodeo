87fef64f3639027d4fd38e1b8280e835625c43cf
David Pollak <dpp@spicelabs.io>
2025-08-11 09:02:39 -0400

    Removed the gitoid matching function
    Signed-off-by: David Pollak <dpp@spicelabs.io>

--- src/main/scala/io/spicelabs/goatrodeo/Main.scala
+++ src/main/scala/io/spicelabs/goatrodeo/Main.scala
@@ -66,7 +66,6 @@ object Howdy {
       tag: Option[String] = None,
       exclude: Vector[(String, Try[Pattern])] = Vector(),
       threads: Int = 4,
-      hotdog: Option[File] = None,
       tagJson: Option[Dom.Element] = None,
       blockList: Option[File] = None,
       maxRecords: Int = 50000,
@@ -106,11 +105,6 @@ object Howdy {
               .filter(f => f.exists())
           )
         ),
-      opt[File]('m', "hotdog")
-        .text(
-          "match the gitoids in this file with the gitoids generated from the input files and outputs the files containing the matches https://youtu.be/ACmydtFDTGs"
-        )
-        .action((x, c) => c.copy(hotdog = Some(x))),
       opt[String]("tag")
         .text(
           "Tag all top level artifacts (files) with the current date and the text of the tag"
@@ -336,19 +330,9 @@ object Howdy {
           return
         }
 
-        val preWriteDB: Storage => Boolean = params.hotdog match {
-          case Some(dog) =>
-            val contents = Files.readString(dog.toPath())
-
-            val toFind = Json
-              .decode(contents.getBytes("UTF-8"))
-              .to[Map[String, Vector[String]]]
-              .value
-
-            s => {
-              emitFound(s, toFind)
-              false
-            }
+        // Placeholder for emitting JSON of ADGs and roots of ADGs...
+        // but it's blank for now
+        val preWriteDB: Storage => Boolean = true match {
 
           case _ => _ => true
         }
