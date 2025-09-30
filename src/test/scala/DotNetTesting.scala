344e2cf6bb9c047bdaf17436a895deb7d899e83c
David Pollak <10572+dpp@users.noreply.github.com>
2025-09-30 11:12:33 -0400

    pURL failures from static metadata logged as debug. (#170)
    * pURL failures from static metadata logged as debug. Started a block list for static metadata types
    
    Signed-off-by: David Pollak <dpp@spicelabs.io>
    
    * Run scalafmt and scalafix. Removed the begining of isBlocked
    
    Signed-off-by: David Pollak <dpp@spicelabs.io>
    
    ---------
    
    Signed-off-by: David Pollak <dpp@spicelabs.io>

--- src/test/scala/DotNetTesting.scala
+++ src/test/scala/DotNetTesting.scala
@@ -1,27 +1,25 @@
-import io.spicelabs.goatrodeo.GoatRodeoBuilder
-import org.apache.tika.config.TikaConfig
+import io.spicelabs.goatrodeo.util.ArtifactWrapper
 import org.apache.tika.io.TikaInputStream
 import org.apache.tika.metadata.Metadata
 import org.apache.tika.metadata.TikaCoreProperties
-import org.apache.tika.mime.MediaType
-import io.spicelabs.goatrodeo.util.ArtifactWrapper
+
 import java.io.File
 
 class DotNetTesting extends munit.FunSuite {
-    test("get-me-a-mime") {
-        val path = "test_data/Smoke.dll"
-        val metadata = new Metadata()
-        metadata.set(TikaCoreProperties.RESOURCE_NAME_KEY, path)
-        val input = TikaInputStream.get(File(path), metadata)
-        val mime = ArtifactWrapper.mimeTypeFor(input, path)
-        assertEquals("application/x-msdownload; format=pe32-dotnet", mime)
-    }
-    test("get-me-a-mime-exe") {
-        val path = "test_data/hackproj.dll"
-        val metadata = new Metadata()
-        metadata.set(TikaCoreProperties.RESOURCE_NAME_KEY, path)
-        val input = TikaInputStream.get(File(path), metadata)
-        val mime = ArtifactWrapper.mimeTypeFor(input, path)
-        assertEquals("application/x-msdownload; format=pe32-dotnet", mime)
-    }
+  test("get-me-a-mime") {
+    val path = "test_data/Smoke.dll"
+    val metadata = new Metadata()
+    metadata.set(TikaCoreProperties.RESOURCE_NAME_KEY, path)
+    val input = TikaInputStream.get(File(path), metadata)
+    val mime = ArtifactWrapper.mimeTypeFor(input, path)
+    assertEquals("application/x-msdownload; format=pe32-dotnet", mime)
+  }
+  test("get-me-a-mime-exe") {
+    val path = "test_data/hackproj.dll"
+    val metadata = new Metadata()
+    metadata.set(TikaCoreProperties.RESOURCE_NAME_KEY, path)
+    val input = TikaInputStream.get(File(path), metadata)
+    val mime = ArtifactWrapper.mimeTypeFor(input, path)
+    assertEquals("application/x-msdownload; format=pe32-dotnet", mime)
+  }
 }
